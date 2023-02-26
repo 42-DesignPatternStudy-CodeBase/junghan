0c56a62ba9094ad574ec698272f6ce9864c7254b
한중명 <gkswndaud@gmail.com>
2023-02-26 19:06:19 +0900

    implement bridgePattern

---
+++ src/bridgePattern/impl/AbstractList.java
@@ -0,0 +1,11 @@
+package bridgePattern.impl;
+
+public interface AbstractList<T> {
+	
+	public void addElement(T obj);
+	public T deleteElement(int i);
+	public int insertElement(T obj, int i);
+	public T getElement(int i);
+	public int getElementSize();
+
+}
