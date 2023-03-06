3a895505bed16d39be72a0ceb0ca80617557d63f
한중명 <gkswndaud@gmail.com>
2023-03-06 16:32:20 +0900

    implements VisitorPattern

--- src/visitorPattren/Entry.java
+++ src/visitorPattren/Entry.java
@@ -1,15 +1,17 @@
 package visitorPattren;
 
-public abstract class Entry {
-	  public abstract String getName();
-	    public abstract int getSize();
-	    public Entry add(Entry entry) throws FileTreatmentException {
-	        throw new FileTreatmentException();
-	    }
-	    public Iterator iterator() throws FileTreatmentException {
-	        throw new FileTreatmentException();
-	    }
-	    public String toString() {
-	        return getName() + " (" + getSize() + ")";
-	    }
+import java.util.Iterator;
+
+public abstract class Entry implements Acceptor{
+	public abstract String getName();
+	public abstract int getSize();
+	public Entry add(Entry entry) throws FileTreatmentException {
+		throw new FileTreatmentException();
+	}
+	public Iterator<Entry> iterator() throws FileTreatmentException {
+		throw new FileTreatmentException();
+	}
+	public String toString() {
+		return getName() + " (" + getSize() + ")";
+	}
 }
---
+++ src/visitorPattren/FileTreatmentException.java
@@ -0,0 +1,19 @@
+package visitorPattren;
+
+
+public class FileTreatmentException extends Exception {
+
+	FileTreatmentException() {
+
+	}
+
+	FileTreatmentException(String message) {
+		super(message);
+	}
+
+	@Override
+	public void printStackTrace() {
+		super.printStackTrace();
+	}
+	
+}
