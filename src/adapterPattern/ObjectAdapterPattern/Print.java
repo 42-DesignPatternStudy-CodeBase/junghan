375c4bfca373d6a827234eeb0769177891bd163b
한중명 <gkswndaud@gmail.com>
2023-03-06 16:31:57 +0900

    implements ObjectAdapterPattern

---
+++ src/adapterPattern/ObjectAdapterPattern/Print.java
@@ -0,0 +1,6 @@
+package adapterPattern.ObjectAdapterPattern;
+
+public interface Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
---
+++ src/adapterPattern/ObjectAdapterPattern/PrintBanner.java
@@ -0,0 +1,20 @@
+package adapterPattern.ObjectAdapterPattern;
+
+public class PrintBanner implements Print{
+	private Banner banner;
+
+	public PrintBanner(String string) {
+		banner = new Banner(string);
+	}
+
+	@Override
+	public void printWeak() {
+		banner.showWithParen();
+	}
+
+	@Override
+	public void printStrong() {
+		banner.showWithAster();
+	}
+
+}
